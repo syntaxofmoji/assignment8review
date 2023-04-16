47878c9468cec3f9a05fe845eaf044466b04bef5
syntaxofmoji <mojivariya@gmail.com>
2023-04-16 23:26:31 +0700

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "../style/Profile.css"
+
+function Profile(){
+    return(
+        <section id = "profile">
+        <h2>Profile</h2>
+        <p>nintendog</p>
+        </section>
+    )
+}
+
+export default Profile
